1046dda869fb923e9006fc828ffce865fa28d7a5
Milttreonna Owens <mowens@basecampcodingacademy.org>
2017-03-21 15:51:29 -0500

    adding Instrument class

--- instrurent_app/models.py
+++ instrurent_app/models.py
@@ -3,3 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Instrument(models.Model):
+    INSTRUMENT_CHOICES=(
+        ('clarinet','Clarinet'),
+        ('acoustic guitar','Acoustic Guitar'),
+        ('banjo','Banjo'),
+    ('conga','Conga Set'),
+    ('drum','Drum Set'),
+    ('electric guitar','Electric Guitar'),
+    ('piano','Piano'),
+    ('sax','saxophone'),
+    ('trumpet','Trumpet'),
+    ('violin','Violin')
+    )
+
+    instrument_choice= models.CharField(max_length="25", choices=INSTRUMENT_CHOICES)
