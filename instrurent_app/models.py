fbef7dab98e182602691f8c3c971df13f3969393
Milttreonna Owens <mowens@basecampcodingacademy.org>
2017-03-24 12:13:01 -0500

    Made it do something(?)

--- instrurent_app/models.py
+++ instrurent_app/models.py
@@ -2,20 +2,59 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
-class Instrument(models.Model):
+
+# http://localhost:8000/instrurent_app/index right
+# # Create your models here.
+class Inventory(models.Model):
     INSTRUMENT_CHOICES=(
-        ('clarinet','Clarinet'),
-        ('acoustic guitar','Acoustic Guitar'),
-        ('banjo','Banjo'),
+    ('clarinet','Clarinet'),
+    ('acoustic guitar','Acoustic Guitar'),
+    ('banjo','Banjo'),
     ('conga','Conga Set'),
     ('drum','Drum Set'),
     ('electric guitar','Electric Guitar'),
     ('piano','Piano'),
-    ('sax','saxophone'),
+    ('sax','Saxophone'),
     ('trumpet','Trumpet'),
     ('violin','Violin')
     )
 
+    instrument_choice= models.CharField(max_length=25, choices=INSTRUMENT_CHOICES)
+    def __str__(self):
+        return self.instrument_choice
+
+# #try __init__
+
+#     def __str__(self,name,instrument_choice):
+#         # self.name=name 
+#         return self.name, self.instrument_choice 
+# # class Choice(models.Model):
+
+
+# #Example  
+
+# class Inventory:
+#     def __init__(self , price, quantity, name):
+#         self.price=price
+#         self.quantity=quantity
+#         self.name=name
     
-    instrument_choice= models.CharField(max_length="25", choices=INSTRUMENT_CHOICES)
+#     def sell(self):
+#         self.quantity-=1
+
+# snickers= Inventory(1.60, 2, "Snickers")
+# snickers.sell()
+# snickers.quantity()
+
+# class Book(models.Model):
+#     author=models.CharField(max_length=200)
+#     title=models.CharField(max_length=200)
+#     price=models.IntegerField()
+
+#     def __str__(self):
+#         return self.author
+
+
+# book=Book("John Greene", "TFIOS")
+# book.title()
+# book.authors()
--- instrurent_app/views.py
+++ instrurent_app/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+# from .models import Book
+from .models import Inventory
 # Create your views here.
 
+# def index(request):
+#     books=Book.objects.all()
+#     return render(request, "rentPage/index.html", {'books':books})
+
+
 def index(request):
-    return render(request, "rentPage/index.html")
+    instruments=Inventory.objects.all()
+    return render(request, "rentPage/index.html", {'instruments':instruments})
 
 def rent(request):
     return render(request, "rentPage/rent.html")
