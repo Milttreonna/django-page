f8ff3c41ddae6d5056729201e6dfe8c5b1fcf067
Milttreonna Owens <mowens@basecampcodingacademy.org>
2017-04-05 09:58:57 -0500

    removing book example

--- instrurent_app/views.py
+++ instrurent_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-# from .models import Book
 from .models import Instrument
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
