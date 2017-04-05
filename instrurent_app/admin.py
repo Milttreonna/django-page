8d0766ecb889009cbe5869885b805414e75d7134
Milttreonna Owens <mowens@basecampcodingacademy.org>
2017-04-05 12:47:01 -0500

    added instrument_price to models

--- instrurent_app/admin.py
+++ instrurent_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # from .models import Book 
 from .models import Instrument
+from django.contrib.admin import register, ModelAdmin
 
 # Register your models here.
 # admin.site.register(Book)
--- instrurent_app/models.py
+++ instrurent_app/models.py
@@ -9,6 +9,7 @@ class Instrument(models.Model):
     instrument_choice= models.CharField(max_length=25)
     instrument_quantity= models.IntegerField(default=200)
     instrument_description = models.CharField(max_length= 2000)
+    instrument_price= models.IntegerField(max_length=None)
 
     def __str__(self):
         return self.instrument_choice
