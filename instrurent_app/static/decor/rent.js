8b8b917305ac99fb12cb821a51a70a61c0936720
Milttreonna Owens <mowens@basecampcodingacademy.org>
2017-04-05 15:54:25 -0500

    item_total()

--- instrurent_app/static/decor/rent.js
+++ instrurent_app/static/decor/rent.js
@@ -8,9 +8,3 @@ function full_total(c) {return (weeks_value*c*1.07);
 }
 
 function show_cart_total() {alert("Thank you for renting with us! Your total was: $"+add_comma(cart_total)); }
-
-var item_total= function(){
-  weeks_value= document.getElementById("Weeks").value;
-  customer_total=full_total(instrument_cost).toFixed(2);
-  document.getElementById("Total").innerHTML="$"+customer_total;
-}
