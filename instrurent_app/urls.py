f362fd873083f6b59e3ae613ea2830d96509492b
Milttreonna Owens <mowens@basecampcodingacademy.org>
2017-04-05 07:04:35 -0500

    added app name

--- instrurent_app/urls.py
+++ instrurent_app/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 
 from . import views
 
+app_name="instrurent"
 
 
 urlpatterns = [ 
     url(r'^index$', views.index, name='index'),
     url(r'^rent$', views.rent, name='rent'),
+    
     
 ]
