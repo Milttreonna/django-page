74df7a8004fd5fc37d1c813fa82d53f682ec6d10
Milttreonna Owens <mowens@basecampcodingacademy.org>
2017-04-05 14:54:31 -0500

    added return 2 format_price, added it to rent.html

--- instrurent_app/models.py
+++ instrurent_app/models.py
@@ -9,8 +9,13 @@ class Instrument(models.Model):
     instrument_choice= models.CharField(max_length=25)
     instrument_quantity= models.IntegerField(default=200)
     instrument_description = models.CharField(max_length= 2000)
+
     instrument_price= models.FloatField(max_length=None)
     instrument_image=models.CharField(max_length=300)
+
+    def format_price(self):
+        instrument_price= round(self.instrument_price,2)
+        return instrument_price
 
     def show_image(self):
         return 'decor/images/' + self.instrument_image
