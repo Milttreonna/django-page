2a81fa2012593ba410557a802535ff96fcc37eea
Milttreonna Owens <mowens@basecampcodingacademy.org>
2017-04-05 09:26:56 -0500

    removing book example

--- instrurent_app/views.py
+++ instrurent_app/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-# from .models import Book
 from .models import Instrument
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 # Create your views here.
-
-# def index(request):
-#     books=Book.objects.all()
-#     return render(request, "rentPage/index.html", {'books':books})
-
 
 def index(request):
     return render(request, "rentPage/index.html")
