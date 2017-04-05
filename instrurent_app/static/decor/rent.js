34a1dfe0688fe5c1b20f0d1709284779803d82bf
Milttreonna Owens <mowens@basecampcodingacademy.org>
2017-04-05 15:54:39 -0500

    clearing js

--- instrurent_app/static/decor/rent.js
+++ instrurent_app/static/decor/rent.js
@@ -1,10 +1,1 @@
 
-
-function add_comma(x) {
-     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
- }
-
-function full_total(c) {return (weeks_value*c*1.07);
-}
-
-function show_cart_total() {alert("Thank you for renting with us! Your total was: $"+add_comma(cart_total)); }
